2e3c57154fccc52b3ca6ef1ae9736436e3beda0b
Matt Tierney <tierney@cs.nyu.edu>
2012-02-04 11:54:41 -0500

    Fixing copy and paste formatting error.

--- scatter2d_heatmap.py
+++ scatter2d_heatmap.py
@@ -74,7 +74,7 @@ def main(argv):
   try:
     argv = FLAGS(argv) # Parse flags.
   except gflags.FlagsError, e:
-    print '%s\\nUsage: %s ARGS\\n%s' % (e, sys.argv[0], FLAGS)
+    print '%s\nUsage: %s ARGS\n%s' % (e, sys.argv[0], FLAGS)
     sys.exit(1)
 
   plot_data_from_filename(FLAGS.filename)
